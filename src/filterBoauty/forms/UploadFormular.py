03ced19ae9326cf8d9f8e39d428396f00c5fb5c0
gurvanrenault <renault.gurvan@live.com>
2024-07-03 15:59:31 +0200

    Test + renommage + doc

--- src/filterBoauty/forms/UploadFormular.py
+++ src/filterBoauty/forms/UploadFormular.py
@@ -1,5 +1,9 @@
 from django import forms
 from django.core.validators import FileExtensionValidator
+
+"""
+     Formulaire d'upload d'image 
+"""
 class UploadFormular(forms.Form):
 
      file = forms.ImageField(validators=[FileExtensionValidator(allowed_extensions=['jpeg', 'jpg','png'])])
